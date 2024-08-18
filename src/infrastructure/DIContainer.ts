ccd67fccc88fa20d696902720c23df68da66243e
bhavana1111 <bhavanakondeti2000@gmail.com>
2024-08-18 13:20:57 -0500

    Added DI container and errorHandling middleware

---
+++ src/infrastructure/DIContainer.ts
@@ -0,0 +1,15 @@
+import { InMemoryBookRepository } from "./repositories/InMemoryBookRepository";
+import { GetAllBooks } from "../use-cases/GetAllBooks";
+
+class DIContainer {
+  private static _bookRepository = new InMemoryBookRepository();
+
+  static getBookRepository() {
+    return this._bookRepository;
+  }
+  static getGetAllBooksUseCase() {
+    return new GetAllBooks(this.getBookRepository());
+  }
+}
+
+export { DIContainer };
